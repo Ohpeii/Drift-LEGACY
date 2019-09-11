dba46a25bc7e5904cebc4e7ee823999a691850d7
Tarboun <50683552+Tarboun@users.noreply.github.com>
2019-09-11 14:51:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -503,24 +503,9 @@ bot.on('message' , message => {
 
 
 
-            bot.on('message', message => {
-                if (message.author.bot) return;
-                if (!message.content.startsWith(prefix)) return;
-              
-                let command = message.content.split(" ")[0];
-                command = command.slice(prefix.length);
-              
-                let args = message.content.split(" ").slice(1);
-              
-              
-                  
-              if (command == "say") {
-              let rank = message.guild.member(message.author).roles.find('name', 'Founder');
-              if (!rank) return message.reply(' ')
-                message.channel.send(args.join("  "))
-                  message.delete();
-                }
-              });
+            bot.on('guildMemberAdd', member=> {
+                member.addRole(member.guild.roles.find("name","community"));
+                });
 
 
 
