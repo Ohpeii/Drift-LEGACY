2fb06d141521240911f392e1b059c13350ee088f
Tarboun <50683552+Tarboun@users.noreply.github.com>
2019-10-01 22:46:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -383,7 +383,7 @@ bot.on('message', async(message) => {
                     if(!{body}) return message.channel.send("I broke! Try again.")
                  
                         let mEmbed = new Discord.RichEmbed()
-                        .setAuthor('Malicious Codes is Back', message.guild.iconURL)
+                        .setAuthor('Malicious is Back', message.guild.iconURL)
                         .setImage(body.url)
                         .setTimestamp()
                  
