c25f2dbba9ed868f12dc5f20f02852d3da4064a2
Tarboun <50683552+Tarboun@users.noreply.github.com>
2020-01-21 00:02:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const superagent = require("snekfetch");
 const prefix = "$"
  
 const token = 'NjE5MTEwNjM5NjM3NjI2ODg3.XXDeXw.VcbmQgbjaJbUz8X8ExiHOAT3QmY';
