ea68b6a492b3c94450f0c68110abd4fdd6c2d888
OVER <50683552+Bowlingtoolkits@users.noreply.github.com>
2020-05-11 04:53:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,20 @@
- // هنا  تحط التون علشان الصور تتحرك 
-
-const token = "mfa.Q0C2i613HhLTqqUClgSH8eagWLIlr_zgJjhkybu6mJxb39ZsKwCGDmbeG59VIchn2nf3OzewhUU38xLzYK0J"
-const Discord = require('discord.js')//Toxic Codes // n3k4a is one
-const client = new Discord.Client();//Toxic Codes // n3k4a is one
-client.on('ready', function(){//Toxic Codes // n3k4a is one
- console.log(`Logged in as ${client.user.tag}!`); //Toxic Codes // n3k4a is one
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const moment = require('moment');
+const Canvas = require('canvas');
+const { readFile, readFileSync } = require('fs-nextra');
+const { get } = require('snekfetch');
+const jimp = require('jimp');
+const ms = require("ms");
+const weather = require('weather-js');
+const fs = require('fs');
+const prefix = "$"
+const token = 'mfa.Q0C2i613HhLTqqUClgSH8eagWLIlr_zgJjhkybu6mJxb39ZsKwCGDmbeG59VIchn2nf3OzewhUU38xLzYK0J';
    
    // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293']; // صور اضافيه
    var s = ['483055660209012736','480169573530861578','483055655800930315'];  // صور  الي بتشتغل
     setInterval(function (){//Toxic Codes // n3k4a is one  
-    client.user.setPresence({//Toxic Codes // n3k4a is one
+    bot.user.setPresence({//Toxic Codes // n3k4a is one
  game: { //Toxic Codes // n3k4a is one
     type: 1,//Toxic Codes // n3k4a is one
      url: 'https://www.twitch.tv/Robot',//Toxic Codes // n3k4a is one
@@ -24,4 +29,5 @@ client.on('ready', function(){//Toxic Codes // n3k4a is one
     }, 5000);//سرعه تغير الصور  // n3k4a is one
 });//Toxic Codes // n3k4a is one
 
-client.login('token'); // token
+ bot.login(token);
+
